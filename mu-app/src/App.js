2f3b3c4ed2ee2eeff8e17bff370359043fb787f3
Courtland Bourgeois <courtland@CourtlandsMBP2.eatel.net>
2019-12-03 17:54:11 -0600

    completed through step 2

--- mu-app/src/App.js
+++ mu-app/src/App.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import Cards from './components/Cards';
 import './App.css';
+import axios from 'axios';
 
 class App extends React.Component {
+  state = {
+    myGitHub: []
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://api.github.com/users/Courtland-Bourgeois")
+      .then(res => {
+        console.log(res.data)
+        this.setState({
+          myGitHub: res.data
+        })
+      })
+  }
 
   render() {
     return (
-      <div className="App">
-        <h1>Hello GitHub Friends</h1>
+      <div className="container">
+        <div className="header">
+          <img src="./images/lambdalogo.png" alt="Lambda Logo"/>
+          <span role="img">❤️'s</span>
+          <img src="./images/githublogo.png" alt="GitHub Logo" />
+        </div>
         <input />
         <button>Find Friends!</button>
         <div className="friends">
-          <Cards />
+          <Cards key={this.state.myGitHub.id} card={this.state.myGitHub} />
         </div>
       </div>
     );
